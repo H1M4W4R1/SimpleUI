f929889dfa1119d5adc155a49e4bfae1b383dc8d
Patryk Pastuszka <nov11157@gmail.com>
2025-08-31 00:08:03 +0200

    Added UIScrollbar

---
+++ Components/Interactable/Scrolling/UIScrollbar.cs
@@ -0,0 +1,34 @@
+﻿using Systems.SimpleUserInterface.Components.Objects.Interactable;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Systems.SimpleUserInterface.Components.Interactable.Scrolling
+{
+    [RequireComponent(typeof(Scrollbar))] public abstract class UIScrollbar : UIInteractableObjectBase
+    {
+        [field: SerializeField, HideInInspector] protected Scrollbar scrollbarReference;
+
+        /// <summary>
+        ///     Raises when the scrollbar value changes
+        /// </summary>
+        /// <param name="value">New scrollbar value (0-1)</param>
+        protected abstract void OnScrollbarValueChanged(float value);
+        
+        /// <summary>
+        ///     Checks if the scrollbar is interactable
+        /// </summary>
+        public override bool IsInteractable => scrollbarReference.interactable;
+        
+        /// <summary>
+        ///     Changes the interactable state of the scrollbar
+        /// </summary>
+        public override void SetInteractable(bool interactable) =>
+            scrollbarReference.interactable = interactable;
+
+        protected override void OnValidate()
+        {
+            base.OnValidate();
+            scrollbarReference = GetComponent<Scrollbar>();
+        }
+    }
+}
