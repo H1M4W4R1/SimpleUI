6472b10c4785d1d2908cf4ee3b89593de5ad72c7
Patryk Pastuszka <nov11157@gmail.com>
2025-09-01 05:13:38 +0200

    ModelViewport with example

---
+++ Examples/07. Model Viewport/Scripts/ExampleModelViewport.cs
@@ -0,0 +1,9 @@
+﻿using Systems.SimpleUserInterface.Components.Models;
+
+namespace Systems.SimpleUserInterface.Examples._07._Model_Viewport.Scripts
+{
+    public sealed class ExampleModelViewport : UIModelViewportBase
+    {
+        
+    }
+}
---
+++ Examples/07. Model Viewport/Scripts/ExamplePrimitiveGameObjectProvider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Systems.SimpleUserInterface.Context.Abstract;
+using UnityEngine;
+
+namespace Systems.SimpleUserInterface.Examples._07._Model_Viewport.Scripts
+{
+    public sealed class ExamplePrimitiveGameObjectProvider : ContextProviderBase<GameObject>
+    {
+        /// <summary>
+        ///     List of game objects to be used as models
+        /// </summary>
+        [field: SerializeField] private List<GameObject> GameObjects = new();
+        [field: SerializeField] private int Index { get; set; }
+        
+        public override GameObject GetContext() => GameObjects[Index % GameObjects.Count];
+    }
+}
